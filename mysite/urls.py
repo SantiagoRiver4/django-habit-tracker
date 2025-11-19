493975fc59b8e0fc5bca609e40999e2e3c137bcf
SantiagoRiver4 <santyr2004@gmail.com>
2025-11-19 11:29:17 -0500

    Add redirect from root URL to polls index

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,8 +1,14 @@
-
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    # 👉 Cuando entren a la raíz "/", los mandamos al index de hábitos
+    path("", RedirectView.as_view(pattern_name="polls:index", permanent=False)),
+
+    # Rutas de la app de hábitos
     path("polls/", include("polls.urls")),
+
+    # Admin de Django
     path("admin/", admin.site.urls),
 ]
